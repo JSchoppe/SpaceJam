73089508ad92d44be2aa0b4da44530c79cee77e9
Jethro Schoppenhorst <Jethro.Schoppenhorst@iam.local>
2019-02-22 12:57:47 -0600

    Implemented Basic Movement
    completes movement, values need to be fine tuned

---
+++ SpaceJam/Assets/Code/CameraControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerTarget;
+
+    private Camera cam;
+
+    // This array represents the different camera regions for the game.
+    // The camera will lock into a zone until the player moves to another screen.
+    // These must be the same length!
+    [SerializeField]
+    private Vector2[] cameraMins;
+    [SerializeField]
+    private Vector2[] cameraMaxes;
+
+    private Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = gameObject.GetComponent<Camera>();
+
+        offset = cam.transform.position - playerTarget.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the current camera region.
+        //Vector2[] zone = GetCurrentZone();
+
+
+        cam.transform.position = playerTarget.position + offset;
+    }
+
+    // This code figures out which region of the map the player is currently in.
+    private Vector2[] GetCurrentZone()
+    {
+        // Cycle through all camera zones.
+        for (int i = 0; i < cameraMins.Length; i++)
+        {
+            if (playerTarget.position.x > cameraMins[i].x && playerTarget.position.x < cameraMaxes[i].x)
+            {
+                if (playerTarget.position.y > cameraMins[i].y && playerTarget.position.y < cameraMaxes[i].y)
+                {
+                    return new Vector2[] { cameraMins[i], cameraMaxes[i]};
+                }
+            }
+        }
+
+        Debug.Log("Player escaped camera region!");
+        return new Vector2[] { new Vector2(-1000, -1000), new Vector2(1000,1000)};
+    }
+
+}
---
+++ SpaceJam/Assets/Code/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed;
+
+
+
+    private bool isGrounded = false;
+    private Rigidbody2D playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 forceToAdd = new Vector2();
+
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            forceToAdd += Input.GetAxis("Vertical") * Vector2.up * playerSpeed * Time.fixedDeltaTime;
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            forceToAdd += Input.GetAxis("Horizontal") * Vector2.right * playerSpeed * Time.fixedDeltaTime;
+        }
+
+
+        playerRB.AddForce(forceToAdd, ForceMode2D.Impulse);
+    }
+}
