6b0aaee8e66f2961edc000dce961febb53322237
Jethro Schoppenhorst <Jethro.Schoppenhorst@iam.local>
2019-02-22 15:12:43 -0600

    Meters Implemented
    Meters for oxygen and health implemented.

--- SpaceJam/Assets/Code/PlayerProgress.cs
+++ SpaceJam/Assets/Code/PlayerProgress.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerProgress : MonoBehaviour
 {
+    // This script basically handles everything that
+    // isn't the physics for the player.
+
+
     [HideInInspector]
     public int EngineRoomCollectibles = 0;
     [HideInInspector]
@@ -10,4 +15,40 @@ public class PlayerProgress : MonoBehaviour
     public int LifeSupportCollectibles = 0;
     [HideInInspector]
     public int GeneratorRoomCollectibles = 0;
+
+    [SerializeField]
+    private Slider oxygenSlider;
+    [SerializeField]
+    private int maxOxygenTime;
+
+    [SerializeField]
+    private Slider healthSlider;
+
+    [SerializeField]
+    private float invulnerabilityTime;
+    private float lastHitTime = 0;
+
+    public void Start()
+    {
+        oxygenSlider.maxValue = maxOxygenTime;
+        oxygenSlider.value = maxOxygenTime;
+        healthSlider.maxValue = 100;
+        healthSlider.value = 100;
+    }
+
+    public void Update()
+    {
+        oxygenSlider.value -= Time.deltaTime;
+    }
+
+    // Call this method from other objects to damage the player.
+    public void TakeDamage(int damage)
+    {
+        // This makes sure the player has i-frames.
+        if (Time.time - lastHitTime < invulnerabilityTime)
+        {
+            healthSlider.value -= damage;
+            lastHitTime = Time.time;
+        }
+    }
 }
