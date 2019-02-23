d8e2521a84d7521cd87599d8d974a8a830acba97
Jethro Schoppenhorst <Jethro.Schoppenhorst@iam.local>
2019-02-22 19:07:43 -0600

    Suck Completed

---
+++ SpaceJam/Assets/Code/SuctionPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuctionPoint : MonoBehaviour
+{
+    // The object to be pulled into the point.
+    [SerializeField]
+    private Rigidbody2D bodyToPull;
+    [SerializeField]
+    private float influenceRadius;
+    [SerializeField]
+    private float strength;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // How far away is the player?
+        // 0-1 = inside influence radius,
+        // 1+ = outside influence radius
+        float ratio = Vector2.Distance(bodyToPull.position, transform.position) / influenceRadius;
+
+        if (ratio < 1)
+        {
+            float magnitude = (strength / ratio) * Time.fixedDeltaTime;
+            bodyToPull.AddForce(magnitude * (new Vector2(transform.position.x, transform.position.y) - bodyToPull.position).normalized, ForceMode2D.Impulse);
+        }
+    }
+
+    private void Update()
+    {
+        transform.eulerAngles += Vector3.forward * 500 * Time.deltaTime;
+    }
+}
