ed3c95c3c27824d8df3e812191ca89cf59cf2fe6
Adrian Gilbert Ratuita <adrianratuita@gmail.com>
2019-02-23 11:59:36 -0600

    Fire Hazard

---
+++ SpaceJam/Assets/Code/FluctuatingHazard.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluctuatingHazard : MonoBehaviour
+{
+
+    [SerializeField]
+    int hitDamage;
+    [SerializeField]
+    int knockback;
+
+    [SerializeField]
+    float fluctuationChance;
+
+    [SerializeField]
+    float fluctuationMinimumRange;
+
+    [SerializeField]
+    Vector2 fluctuationRange;
+
+    [SerializeField]
+    GameObject visualReference;
+
+    //
+    CircleCollider2D hitArea;
+
+    //
+    private float currentHitArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //
+        hitArea = this.gameObject.GetComponent<CircleCollider2D>();
+
+        //
+        currentHitArea = Random.Range(fluctuationRange.x, fluctuationRange.y + fluctuationMinimumRange);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Randomized chance of radius expanding
+        // Cast to int to prevent floating values
+        if ((int)Random.Range(0, fluctuationChance + 2) == (int)fluctuationMinimumRange)
+        {
+
+            // Randomize hit area radius
+            currentHitArea = Random.Range(fluctuationRange.x, fluctuationRange.y + fluctuationMinimumRange);
+
+            // Prevent hit area from going beyond max range
+            if (currentHitArea > fluctuationRange.y) currentHitArea = fluctuationRange.y;
+
+        }
+        else
+        {
+       
+            // Expand radius
+            hitArea.radius = Mathf.Lerp(hitArea.radius, currentHitArea, 0.1f);
+
+            // Prepare current scale and final scale
+            Vector3 currentScale = visualReference.transform.localScale;
+            Vector3 finalScale = new Vector3(currentHitArea * 2, currentHitArea * 2, 1);
+
+            // Lerp to Scale
+            visualReference.transform.localScale = Vector3.Lerp(currentScale, finalScale, 0.1f);
+
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        PlayerProgress player = collision.gameObject.GetComponent<PlayerProgress>();
+        Rigidbody2D playerBody = collision.gameObject.GetComponent<Rigidbody2D>();
+        player.TakeDamage(hitDamage);
+
+        playerBody.velocity = (playerBody.transform.position - transform.position) * knockback;
+
+    }
+
+}
